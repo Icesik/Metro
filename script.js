5f2355acfd1268d794a5ed2cccb7c07ce3e0eb73
Icesik <Icesiiik@gmail.com>
2017-02-12 00:16:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,72 @@
+nbSumValue = 70;
+execTimeSystem = null;
+execTimePerso = null;
+nbSumSystemRes = null;
+nbSumPersoRes = null;
+
 runSystem = function (){
-	var execTimeSystem = getFunctionDuration("nbSumPro(70,70)");
+	execTimeSystem = getFunctionDuration("nbSumPro("+nbSumValue+","+nbSumValue+")", true);
 	$('#execTimeSystem').html(execTimeSystem);
 };
 
 runPerso = function(){
-	var execTimeCustom = getFunctionDuration("nbSumPerso(70, 0, 70)");
-	$('#execTimeCustom').html(execTimeCustom);
-  
-  var execTimeSystem = $('#execTimeSystem').html();
-  if(execTimeSystem == "" || execTimeSystem == null || execTimeSystem == "undefined"  		|| execTimeSystem == "-"){
-  		runSystem();
-  		execTimeSystem = $('#execTimeSystem').html();
-  }
-  if(execTimeSystem > 0 && execTimeCustom > 0){
-  		var ratio = Math.round(
-      	(execTimeSystem - execTimeCustom) / execTimeSystem * 10000) / 100;
-      if(ratio < 50){
-      		$('#progressBar').css('background-color', 'red');
-      } else {
-      		$('#progressBar').css('background-color', 'green');
+	execTimePerso = getFunctionDuration("nbSumPerso("+nbSumValue+", 0, "+nbSumValue+")", false);
+      if(execTimeSystem == null){
+          runSystem();
       }
-      $('#progressValue').html(ratio);
-      $('#progressBar').css('width', ratio+'%');
+      
+  if(checkLightPersoIsRight()){
+      $('#execTimeCustom').html(execTimePerso);
+
+      if(execTimeSystem > 0 && execTimePerso > 0){
+          var ratio = Math.round(
+            (execTimeSystem - execTimePerso) / execTimeSystem * 10000) / 100;
+          if(ratio < 50){
+              $('#progressBar').css('background-color', 'red');
+          } else {
+              $('#progressBar').css('background-color', 'green');
+          }
+          $('#progressValue').html(ratio);
+          $('#progressBar').css('width', ratio+'%');
+          if(ratio >= 50){
+          		if(confirm("Test concluant. Lancer le test définitif?")){
+              		if(checkDeepPersoIsRight()){
+                  		alert("Système contourné. Clé obtenue : AHH");
+                  } else {
+                  		$('#execTimeCustom').html('CORRUPTION');
+  										$('#progressValue').html('CORRUPTION');
+                      $('#progressBar').css('background-color', 'red');
+                      alert("Système corrompu, La fonction insérée dans le système ne répond pas aux paramètres.");
+                  }
+              }
+          }
+      }
+  } else {
+      $('#execTimeCustom').html('ERREUR');
+  		$('#progressValue').html('ERREUR');
+    	$('#progressBar').css('width', '0%');
+    	$('#progressBar').css('background-color', 'red');
   }
 };
+
+checkLightPersoIsRight = function(){
+	var isRight = false;
+	if(nbSumSystemRes === nbSumPersoRes){
+  		isRight = true;
+  }
+  return isRight;
+}
+
+checkDeepPersoIsRight = function(){
+		for(var i = 5; i < 50; i++){
+    		var persoRes = nbSumPerso(i, 0, i);
+        var systemRes = nbSumPro(i, i);
+        if(persoRes !== systemRes){
+        		return false;
+        }
+    }
+    return true;
+}
 
 nbSumPro = function(nb, max){
 	var res = 0, i = 0;
@@ -45,9 +88,18 @@ nbSumPro = function(nb, max){
   return res;
 };
 
-getFunctionDuration = function(jsOperations){
-		var timeStart = new Date().getTime();
-    console.log(eval(jsOperations));
+getFunctionDuration = function(jsOperations, isSystem){
+	var timeStart = new Date().getTime();
+    var res = eval(jsOperations);
+    if(isSystem){
+    	nbSumSystemRes = res;
+    } else {
+    	nbSumPersoRes = res;
+    }
     var time = new Date().getTime() - timeStart;
     return time;
 };
+
+$('#triggerRunPerso').click(function(){
+	runPerso();
+});
